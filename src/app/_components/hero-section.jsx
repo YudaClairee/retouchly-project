7076f7014a43d93d975be303f3f19edb602a1356
Djibrani Yuda <djibyuda@gmail.com>
2025-07-28 18:57:23 +0700

    make landing page: not finished yet

---
+++ src/app/_components/hero-section.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div className="max-w-5xl mx-auto space-y-8">
+      <div className="flex flex-col text-center font-[var(--font-sora)]">
+        <h1 className="text-8xl font-bold bg-gradient-to-r from-[#a855f7] to-[#ec4899] bg-clip-text text-transparent">
+          Edit Images. Instantly. Beautifully.
+        </h1>
+        <p className="mt-8 text-retouchly-muted text-3xl font-[var(--font-inter)]">
+          All the tools you need. None of the clutter.
+        </p>
+        <div className="mt-8 flex gap-8 items-center justify-center">
+          <Button className="bg-gradient-to-r from-[#a855f7] to-[#ec4899] p-6 rounded-md text-retouchly-text text-xl font-medium">
+            Try Now
+          </Button>
+          <Button className="bg-[#A855F7]/20 p-6 rounded-md text-retouchly-text text-lg font-medium">
+            Watch Demo
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,64 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import HeroSection from "./_components/hero-section";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is a simple example of a Next.js application.</p>
+    <div className="bg-gradient-to-b from-[#1f2937] to-[#171834] min-h-screen py-10 font-[var(--font-inter)]">
+      <header className="rounded-full py-4 px-8 flex items-center justify-between bg-[#A855F7]/20 max-w-4xl mx-auto ">
+        <div className="flex items-center">
+          <Image
+            src="/retouchly-image.png"
+            width={150}
+            height={100}
+            alt="Retouchly"
+          />
+        </div>
+
+        <nav className="flex items-center gap-6">
+          <ul className="flex gap-4 text-white font-medium text-lg">
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <SignedOut>
+            <SignInButton className="text-white mr-4">Login</SignInButton>
+            <SignUpButton>
+              <Button className="bg-[#EC4899]/80 rounded-full font-semibold text-lg text-white hover:bg-[#EC4899]/90 px-4 py-4 transition-colors cursor-pointer">
+                Register
+              </Button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "bg-[#A855F7]/20 rounded-full size-12",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </header>
+      <main className="max-w-5xl mx-auto mt-18 px-4">
+        <HeroSection />
+      </main>
+      <section></section>
     </div>
   );
 }
